7224d07bd27ae8ef26858f589c948aaa9f3d3364
glitzsfa <glitzsfa@gmail.com>
2022-11-07 19:51:23 +0000

    mixed lists

---
+++ mycode/mix-list/mixlist01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+print("The first item is: " + my_list[0] ) #comment1
+
+print("The 2nd item is: " + str(my_list[1]) ) #comment2
+
+# inline comment
+print("The last item is: " + my_list[2] ) #comment3
+
